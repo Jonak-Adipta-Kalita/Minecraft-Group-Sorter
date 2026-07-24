a0492d9cf4cdec4789226f40c893035b9f4e2ae1
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2026-07-24 09:43:53 +0530

    recipe for the controller

--- src/main/java/jak/groupsorter/datagen/ModRecipeProvider.java
+++ src/main/java/jak/groupsorter/datagen/ModRecipeProvider.java
@@ -9,10 +9,12 @@ import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.CookingBookCategory;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
 import org.jspecify.annotations.NonNull;
 
 import java.util.List;
@@ -55,6 +57,17 @@ public class ModRecipeProvider extends RecipeProvider {
             .unlockedBy(getHasName(ModBlocks.AZURITE_BLOCK.get()), has(ModBlocks.AZURITE_BLOCK))
             .group("azurite")
             .save(output, "jakgroupsorter:azurite_from_azurite_block");
+
+        shaped(RecipeCategory.MISC, ModBlocks.CONTROLLER.get())
+            .pattern("III")
+            .pattern("ARA")
+            .pattern("AAA")
+            .define('A', ModBlocks.AZURITE_BLOCK.get())
+            .define('I', Items.IRON_INGOT)
+            .define('R', Blocks.REDSTONE_BLOCK)
+            .unlockedBy(getHasName(ModBlocks.AZURITE_BLOCK.get()), has(ModBlocks.AZURITE_BLOCK))
+            .group("chest_room_controller")
+            .save(output);
 
         List<ItemLike> AZURITE_SMELTABLES = List.of(ModItems.RAW_AZURITE, ModBlocks.AZURITE_ORE, ModBlocks.AZURITE_DEEPSLATE_ORE);
         oreSmelting(AZURITE_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.MISC, ModItems.AZURITE.get(), 0.25f, 200, "azurite");
